efea2d46322c92f69461ed883da251fb67bff709
dfariaor-lgtm <dfariaor@gmail.com>
2026-01-20 11:30:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-require("http")
-  .createServer((req, res) => {
-    res.end("ok");
-  })
-  .listen(process.env.PORT || 3000);
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend LawFlow rodando 🚀");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Servidor rodando na porta " + PORT);
+});
